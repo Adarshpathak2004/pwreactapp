ba7409744f9393bb023689a0cf98a9cfccc01194
Adarshpathak2004 <23je0034@iitism.ac.in>
2026-03-12 20:05:00 +0530

    last changes

--- src/api.js
+++ src/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const API = axios.create({
-  baseURL: import.meta.env.API, // Express backend
+  baseURL: import.meta.env.VITE_API, // Express backend
 });
 
 // Attach token automatically if available
